bd7b308713b1207cd977300a80a8f7db4ce198d4
HopinNotDisaster <syq3315813594@gmail.com>
2024-04-30 13:06:18 +0800

    添加管理！

--- mysite/operate/admin.py
+++ mysite/operate/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import UserCollects
+
 
 # Register your models here.
+
+@admin.register(UserCollects)
+class UserCollects(admin.ModelAdmin):
+    pass
